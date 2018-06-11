e41325a7ab433775c2840626c6261d73121463b6
NamiDorama <yulliavoitiuk@gmail.com>
2018-06-11 13:32:02 +0300

    a
    a

--- src/components/twit/Twit.js
+++ src/components/twit/Twit.js
@@ -1,12 +1,18 @@
 import React from 'react'
 
 const Twit = (props) => (
-    <div>
+    <div className = 'twit'>
         <span className="name">{props.name}</span>
         <br/>
         <span>{props.post}</span>
         <br/>
-        <span>{new Date(props.date).getHours()}</span>:<span>{new Date(props.date).getMinutes()}</span> <span>{new Date(props.date).getDate()}</span>.<span>{new Date(props.date).getMonth()}</span>
+        <div className="date">
+            <span>{new Date(props.date).getHours()}</span>:
+            <span>{new Date(props.date).getMinutes()}</span>
+            <span>{new Date(props.date).getDate()}</span>.
+            <span>{new Date(props.date).getMonth()}</span>
+        </div>
+
     </div>
 )
 
--- src/components/twit/TwitGrid.js
+++ src/components/twit/TwitGrid.js
@@ -24,6 +24,7 @@ class TwitGrid extends Component {
                         post={e.post}
                         date={e.date}
                         key={new Date(e.date)}
+
                     />
                 ))}
             </div>
